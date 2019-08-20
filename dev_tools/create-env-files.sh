57185c20c18547cbda1849687ca2b91c0e1cb10e
Bernhard Walter <bernhard.walter@databricks.com>
2019-08-20 10:14:07 +0200

    Move release logic to Makefile

--- dev_tools/create-env-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-prefix=dbr-env-file-plugins
-
-for plugin in $(ls $prefix/*.yml-plugin); do
-    filename=$(basename $plugin)
-    cat $prefix/env-master.yml $plugin > "databrickslabs_jupyterlab/env_files/env-${filename%.*}.yml"
-done
