a8af3d046d60a8fe2abe65deb205839480dacb34
Bernhard Walter <bernhard.walter@databricks.com>
2019-08-20 22:13:54 +0200

    Latest release: 1.0.0-rc4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "databrickslabs_jupyterlab",
-    version = "1.0.0-rc3",
+    version = "1.0.0-rc4",
     author = "Bernhard Walter",
     author_email = "bernhard.walter@databricks.com",
     description = ("Remote Jupyter Lab kernel for Databricks"),
