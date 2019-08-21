3ec2d558242a083d95a971ce9076465bc4fae3ee
Bernhard Walter <bernhard.walter@databricks.com>
2019-08-21 17:10:28 +0200

    interims version for bumpversion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "databrickslabs_jupyterlab",
-    version = "1.0.1-rc0",
+    version = "0.9.0",
     author = "Bernhard Walter",
     author_email = "bernhard.walter@databricks.com",
     description = ("Remote Jupyter Lab kernel for Databricks"),
