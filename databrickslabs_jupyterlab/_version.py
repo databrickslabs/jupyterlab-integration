1af9a438bc6e5dbf524b4dcef68e766bb68fb949
Bernhard Walter <bernhard.walter@databricks.com>
2019-09-04 18:53:11 +0200

    Latest release: 1.0.2-rc6

--- databrickslabs_jupyterlab/_version.py
+++ databrickslabs_jupyterlab/_version.py
@@ -10,5 +10,5 @@ def get_version(version):
     return VersionInfo(major, minor, patch, release, build)
 
 
-__version__ = "1.0.2-rc5"  # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
+__version__ = "1.0.2-rc6"  # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
 __version_info__ = get_version(__version__)
