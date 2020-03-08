be4864c61ac224425a12813632ae44f1bf845db6
Bernhard <b_walter@arcor.de>
2020-03-08 22:25:57 +0100

    adapt to jupyterlab 2.0

--- extensions/databrickslabs_jupyterlab_statusbar/src/dbstatus.tsx
+++ extensions/databrickslabs_jupyterlab_statusbar/src/dbstatus.tsx
@@ -1,21 +1,13 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
 import React from 'react';
-
-import { VDomRenderer, VDomModel } from '@jupyterlab/apputils';
-
-import { URLExt, Poll } from '@jupyterlab/coreutils';
-
+import { VDomRenderer, VDomModel, ISessionContext } from '@jupyterlab/apputils';
+import { URLExt } from '@jupyterlab/coreutils';
 import { interactiveItem, TextItem } from '@jupyterlab/statusbar';
-
 import { ServerConnection } from '@jupyterlab/services';
-
 import { ILabShell } from '@jupyterlab/application';
-
 import { INotebookTracker } from '@jupyterlab/notebook';
-
 import { Dialog, showDialog } from '@jupyterlab/apputils';
+import { Poll } from '@lumino/polling';
+import { NotebookPanel } from '@jupyterlab/notebook';
 
 
 class Counter {
@@ -31,26 +23,28 @@ class Counter {
     var result = (this._count % this._delay == 0)
     if ((this._limit != -1) && (this._count > this._limit)) {
       switch (this._delay) {
-        case 1: { 
-          this._delay = 2; 
+        case 1: {
+          this._delay = 2;
           this._limit = 30; // 60 seconds every 2 seconds
-          break; } 
-        case 2: { 
-          this._delay = 5; 
-          this._limit = 24; // 120 seconds every 5 seconds
-          break; }
-        case 5: { 
-          this._delay = 10;  // every 10 seconds until reset
-          this._limit = -1;
-          break; 
+          break;
         }
-        default: { 
+        case 2: {
+          this._delay = 5;
+          this._limit = 24; // 120 seconds every 5 seconds
+          break;
+        }
+        case 5: {
           this._delay = 10;  // every 10 seconds until reset
           this._limit = -1;
-          break; 
+          break;
+        }
+        default: {
+          this._delay = 10;  // every 10 seconds until reset
+          this._limit = -1;
+          break;
         }
       }
-      
+
       this._count = 1;
       console.debug("Databrickslabs-jupyterlab: monitoring every", this._delay, "seconds")
     }
@@ -70,12 +64,11 @@ export class DbStatus extends VDomRenderer<DbStatus.Model> {
    * Construct a new databricks status item.
    */
   constructor(labShell: ILabShell, notebookTracker: INotebookTracker) {
-    super();
-    this.model = new DbStatus.Model({
+    super(new DbStatus.Model({
       refreshRate: 1000,
       labShell: labShell,
       notebookTracker: notebookTracker
-    });
+    }));
     this.addClass(interactiveItem);
   }
 
@@ -87,14 +80,14 @@ export class DbStatus extends VDomRenderer<DbStatus.Model> {
       return null;
     }
     let text: string = this.model.currentStatus;
-    if (text === "UNREACHABLE") { 
+    if (text === "UNREACHABLE") {
       text = "[ " + text + " ] (click here to restart)";
-    } else if (text === "CONNECT FAILED") { 
+    } else if (text === "CONNECT FAILED") {
       text = "[ " + text + " ] (retrying)";
     } else {
       text = "[ " + text + " ]";
     }
-    return <TextItem onClick={() => this.model.handleClick()} title="Current databricks status" source={text} />;    
+    return <TextItem onClick={() => this.model.handleClick()} title="Current databricks status" source={text} />;
   }
 }
 
@@ -115,18 +108,37 @@ export namespace DbStatus {
       super();
       this._notebookTracker = options.notebookTracker;
 
-      options.labShell.restored.then((layout) => { 
-        var session = options.notebookTracker.currentWidget.session;
-        console.debug("Databrickslabs-jupyterlab: session", session)
-        session.kernelChanged.connect((change) => {
-          console.debug("Databrickslabs-jupyterlab: Kernel changed", change)
-          this.check_status(session.kernelDisplayName, session.kernel.id)
-        })
-        session.ready.then(() => {
-          console.debug("Databrickslabs-jupyterlab: Session ready", session)
-          this.check_status(session.kernelDisplayName, session.kernel.id)
-        })
-      })
+      options.labShell.restored.then(
+        (layout) => {
+          // notebookTracker.currentWidget might be wrong after new start or browser refresh with more 
+          // than 1 open tab. Hence loop through all open tabs to find the visble one
+          this._notebookTracker.forEach((notebookPanel: NotebookPanel) => {
+            if (notebookPanel.isVisible) {
+              console.debug("Databrickslabs-jupyterlab: select open notebook")
+              this.check_status(notebookPanel.sessionContext)
+            }
+          })
+
+          // When a new tab gets selected, update the kernel id
+          this._notebookTracker.currentChanged.connect((sender: INotebookTracker, notebookPanel: NotebookPanel) => {
+            console.debug("Databrickslabs-jupyterlab: selected notebook changed")
+            this.check_status()
+          })
+
+          // When a kernel gets strted, restarted or stopped, update the kernel id
+          options.labShell.currentChanged.connect((_, change) => {
+            const { oldValue, newValue } = change;
+            if (oldValue) {
+              var context = (oldValue as NotebookPanel).sessionContext
+              context.connectionStatusChanged.disconnect(this.onConnectionStatusChange, this)
+            }
+            if (newValue) {
+              var context = (newValue as NotebookPanel).sessionContext
+              context.connectionStatusChanged.connect(this.onConnectionStatusChange, this)
+            }
+          })
+        }
+      )
 
       this._poll = new Poll<Private.IDbStatusRequestResult | null>({
         factory: () => Private.factory(this, options.notebookTracker),
@@ -155,33 +167,59 @@ export namespace DbStatus {
           return;
         }
       });
+    }
 
-      options.labShell.currentChanged.connect((_, change) => {
-        console.debug("Databrickslabs-jupyterlab: shell changed")
+
+    cleanup() {
+      console.info("Databrickslabs-jupyterlab: cleanup")
+      this._updateStatusValues(null)
+    }
+
+    private onConnectionStatusChange(context: ISessionContext, change: any) {
+      console.debug("Databrickslabs-jupyterlab: kernel state changed: " + change)
+      if (change == "disconnected") {
+        this.cleanup()
+      } else if (change == "connected") {
+        this.check_status()
+      }
+    }
+
+    private async check_status(context: ISessionContext = null) {
+      if (!context) {
+        var context = this._notebookTracker.currentWidget.sessionContext;
+        // console.debug("Databrickslabs-jupyterlab: check status, set: ", context)
+      }
+      // console.debug("Databrickslabs-jupyterlab: check status: ", context)
+      context.ready.then(async () => {
+        var name = context.kernelDisplayName
+        var id = context.session.kernel.id
+
+        const response = await Private.request("databrickslabs-jupyterlab-status", name, id);
+        // console.debug("Databrickslabs-jupyterlab: check status, response: ", response)
+
+        if (response && response.ok) {
+          var result = await response.json();
+          console.debug("Databrickslabs-jupyterlab: check status, result: ", result)
+          if (result.status === "UNREACHABLE") {
+            this._updateStatusValues(result)
+          }
+        } else {
+          if (response) {
+            console.error("check_status response: unknown", response)
+          }
+        }
       })
     }
 
-    private async check_status(name:string, id: string) {
-      const response = await Private.request("databrickslabs-jupyterlab-status", name, id);
-      if (response.ok) {
-        var result = await response.json();
-        if (result.status === "UNREACHABLE") {
-          this._updateStatusValues(result)
-        }
-      } else {
-        console.error("check_status response: unknown", response)
-      }
-    }
-    
-    private _restart_dialog(status:string) {
-      if (! this._dialog_shown) {
+    private _restart_dialog(status: string) {
+      if (!this._dialog_shown) {
         this._dialog_shown = true;
         var title, body, label;
         if (status === "UNREACHABLE") {
           title = "Cluster not reachable";
           body = "Cluster cannot be reached. Check cluster or your network (e.g. VPN) and then press 'Restart' to restart the kernel or cluster";
           label = "Restart";
-        } 
+        }
         showDialog({
           title: title,
           body: body,
@@ -191,9 +229,10 @@ export namespace DbStatus {
           ]
         }).then(result => {
           if (result.button.accept) {
-            let session = this._notebookTracker.currentWidget!.session;
-            let id = session.kernel.id;
-            let name = session.kernelDisplayName;
+            let context = this._notebookTracker.currentWidget.sessionContext;
+            this._notebookTracker.currentWidget.sessionContext.kernelDisplayName
+            let id = context.session.kernel.id;
+            let name = context.kernelDisplayName;
             Private.request("/databrickslabs-jupyterlab-start", name, id)
             this.counter.reset();
           }
@@ -207,6 +246,9 @@ export namespace DbStatus {
     private _updateStatusValues(value: Private.IDbStatusRequestResult | null): void {
       const oldStatusAvailable = this._statusAvailable;
       const oldCurrentStatus = this._currentStatus;
+      console.debug("Databrickslabs-jupyterlab: (old status avail, old current status, value)",
+        oldStatusAvailable, oldCurrentStatus, value
+      )
       if (value === null) {
         this._statusAvailable = false;
         this._currentStatus = "";
@@ -221,7 +263,7 @@ export namespace DbStatus {
           this._restart_dialog(this._currentStatus)
         }
       }
-  
+
       if (
         this._currentStatus !== oldCurrentStatus ||
         this._statusAvailable !== oldStatusAvailable
@@ -235,7 +277,7 @@ export namespace DbStatus {
      */
     handleClick() {
       if (this._currentStatus == "UNREACHABLE") {
-          this._restart_dialog(this.currentStatus)
+        this._restart_dialog(this.currentStatus)
       }
     }
 
@@ -315,40 +357,44 @@ namespace Private {
     if (name && name.slice(0, 4) != "SSH ") {
       return null
     }
+    console.debug("Databrickslabs-jupyterlab: request ", command, name, id)
     var regex = new RegExp("^SSH (.+) ([^:]+):(.*)");
     var parts = regex.exec(name);
     var cluster_id = parts[1];
     var profile = parts[2];
-    
+
     var url = URLExt.join(SERVER_CONNECTION_SETTINGS.baseUrl, command);
     url = url + "?profile=" + profile + "&cluster_id=" + cluster_id + "&id=" + id;
+    // console.debug("Databrickslabs-jupyterlab: request url: ", url)
 
     return ServerConnection.makeRequest(url, {}, SERVER_CONNECTION_SETTINGS);
   }
-  
+
   export async function factory(parent: DbStatus.Model, notebookTracker: INotebookTracker): Promise<IDbStatusRequestResult | null> {
-    
-    let name = notebookTracker.currentWidget!.session.kernelDisplayName;
+
+    let name = notebookTracker.currentWidget.sessionContext.kernelDisplayName;
     var tab_switch = old_name !== name;
     old_name = name;
-    let id = notebookTracker.currentWidget!.session.kernel.id;
+    let id = notebookTracker.currentWidget.sessionContext.session.kernel.id;
     var is_starting = parent.currentStatus !== "Connected";
     var do_status_check = parent.counter.next()
 
     if (do_status_check || is_starting || tab_switch) {
-      
+
       const response = await request("databrickslabs-jupyterlab-status", name, id);
+      // console.debug("Databrickslabs-jupyterlab: factory, response: ", response)
 
       if (response.ok) {
         try {
           var result = await response.json();
+          // console.debug("Databrickslabs-jupyterlab: factory, result: ", result)
           return result
         } catch (error) {
           throw error;
         }
-      } 
+      }
     } else {
-      return {"status": parent.currentStatus}
+      return { "status": parent.currentStatus }
     }
     return null;
   }
