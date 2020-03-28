aca52a81c5bc8b170f3d4fffe1467fed75df8fd3
Bernhard Walter <bernhard.walter@databricks.com>
2020-03-28 17:51:40 +0100

    Improved database and dbfs browser

--- databrickslabs_jupyterlab/database.py
+++ databrickslabs_jupyterlab/database.py
@@ -1,130 +1,187 @@
 import os
-from ipywidgets import Accordion, Select, VBox, Button, Output, Layout
+from ipywidgets import Accordion, Select, HBox, VBox, Button, Output, Layout
 from IPython.display import display, HTML
+import pandas as pd
 
 
-class Databases(object):
-    """Singleton for the Databases object"""
+def _px(val):
+    return "%dpx" % val
 
-    instance = None
 
-    class __Databases(object):
-        """Database browser implementation
-            
-        Args:
-            spark (SparkSession): Spark Session object
-        """
+class DBList:
+    def __init__(self, spark, console):
+        self.spark = spark
+        self.console = console
+        self.height = 200
+        self.db = None
+        self.tbl = None
 
-        def __init__(self, spark):
-            self.spark = spark
+    def _set_status(self, status):
+        with self.console:
+            print(status, "...")
 
-        def create(self):
-            self.refresh = Button(description="refresh")
-            self.refresh.on_click(self.on_refresh)
-            self.output = Output(
-                layout=Layout(
-                    height="600px", width="320px", overflow_x="scroll", overflow_y="scroll"
+    def _clear_status(self):
+        self.console.clear_output()
+
+    def create(self, width=120, height=200):
+        self.height = height
+        self.refresh = Button(icon="refresh", layout={"width": "40px"})
+        self.refresh.on_click(self.on_refresh)
+        self.list = Select(
+            options=[], disabled=False, layout={"width": _px(width), "height": _px(height)}
+        )
+        self.list.observe(self.on_click, names="value")
+        self.update()
+
+    def on_refresh(self, b):
+        self.update()
+
+    def on_click(self, change):
+        self.console.clear_output()
+        if change["old"] is not None:
+            selected = change["new"]
+            self.update(selected)
+
+    def update(self, selected=None):
+        pass
+
+
+class DatabaseList(DBList):
+    def __init__(self, spark, console, tbl):
+        super().__init__(spark, console)
+        self.tbl = tbl
+
+    def update(self, db=None):
+        if db is None:
+            self._set_status("loading databases")
+            result = [db.name for db in self.spark.catalog.listDatabases()]
+            self.list.options = [""] + result
+            self._clear_status()
+        else:
+            self.tbl.set_db(db)
+
+
+class TableList(DBList):
+    def __init__(self, spark, console, schema, data, sample_size=20):
+        super().__init__(spark, console)
+        self.schema = schema
+        self.data = data
+        self.sample_size = sample_size
+
+    def _display_schema(self, schema, sep=False):
+        if not sep:
+            self.schema.clear_output()
+        with self.schema:
+            if sep:
+                display(HTML("<hr>"))
+            display(schema)
+
+    def _display_data(self, data):
+        self.data.clear_output()
+        with self.data:
+            display(data)
+
+    def set_db(self, db):
+        self.db = db
+        self.update()
+
+        import pandas as pd
+
+    def to_dict(self, c):
+        return {
+            "name": c.name,
+            "data_type": c.dataType,
+            "description": c.description,
+            "nullable": c.nullable,
+            "is_partition": c.isPartition,
+            "is_bucket": c.isBucket,
+        }
+
+    def update(self, tbl=None):
+        if self.db is None:
+            result = []
+        elif tbl is None:
+            self._set_status("loading tables")
+            result = [tbl.name for tbl in self.spark.catalog.listTables(self.db)]
+            self.list.options = [""] + result
+            self._clear_status()
+        else:
+            # show schema
+            self.schema.clear_output()
+            self.data.clear_output()
+            df = None
+            try:
+                df = pd.DataFrame(
+                    [self.to_dict(col) for col in self.spark.catalog.listColumns(tbl, self.db)]
                 )
-            )
-            self.output.add_class("db-detail")
-            self.selects = []
-            self.accordion = Accordion(children=[])
+            except:
+                self._display_schema("Cannot load schema")
 
-            display(VBox([self.refresh, self.accordion, self.output]))
+            if df is None or df.empty:
+                self._display_schema("Cannot load schema")
+            else:
+                df = df[
+                    ["name", "data_type", "description", "nullable", "is_partition", "is_bucket"]
+                ]
+                self._display_schema(df)
 
-            self.update()
-            self.set_css()
+                # show extended information
+                pdf = self.spark.sql("describe extended %s.%s" % (self.db, tbl)).toPandas()
+                pdf = pdf[pdf.index > len(df) + 1][["col_name", "data_type"]]
+                pdf.columns = ["Key", "Value"]
+                self._display_schema(pdf.style.hide_index(), sep=True)
 
-        def on_refresh(self, b):
-            """Refresh handler
-            
-            Args:
-                b (ipywidgets.Button): clicked button
-            """
-            self.selects = []
-            self.update()
+                # show sample data
+                self._set_status("loading data")
+                try:
+                    result = self.spark.sql(
+                        "select * from %s.%s limit %d" % (self.db, tbl, self.sample_size)
+                    ).toPandas()
+                    self._display_data(result)
+                except:
+                    self._display_data("Cannot load data")
+                self._clear_status()
 
-        def update(self):
-            """Update the view when an element was selected"""
-            tables = {}
-            for obj in self.spark.sql("show tables").rdd.collect():
-                db = obj[0]
-                table = obj[1]
-                temp = obj[2]
-                if temp and db == "":
-                    db = "temp"
-                if tables.get(db, None) is None:
-                    tables[db] = []
-                if temp:
-                    tables[db].append("%s (temp)" % table)
-                else:
-                    tables[db].append(table)
 
-            for db in sorted(tables.keys()):
-                select = Select(options=[""] + sorted(tables[db]), disabled=False)
-                select.observe(self.on_click(db, self), names="value")
-                self.selects.append(select)
-            self.accordion.children = self.selects
-            for i, db in enumerate(sorted(tables.keys())):
-                self.accordion.set_title(i, db)
+class Databases:
+    """Database browser implementation
 
-        def on_click(self, db, parent):
-            """Click handler providing db and parent as context
-            
-            Args:
-                db (str): database name
-                parent (object): parent object
-            """
+    Args:
+        spark (SparkSession): Spark Session object
+    """
 
-            def f(change):
-                if change["old"] is not None:
-                    parent.output.clear_output()
-                    with parent.output:
-                        if db == "temp":
-                            table = change["new"]
-                        else:
-                            table = "%s.%s" % (db, change["new"])
-                        if table.endswith(" (temp)"):
-                            table = table[:-7]
+    def __init__(self, spark):
+        self.spark = spark
 
-                        try:
-                            schema = parent.spark.sql("describe extended %s" % table)
-                            rows = int(parent.spark.conf.get("spark.sql.repl.eagerEval.maxNumRows"))
-                            parent.spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", 1000)
-                            display(schema)
-                            parent.spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", rows)
-                        except:
-                            print("schema cannot be accessed, table most probably broken")
-
-            return f
-
-        def close(self):
-            """Close view"""
-            self.selects = []
-
-        def set_css(self):
-            """Set CSS"""
-            display(
-                HTML(
-                    """
-            <style>
-            .db-detail .p-Widget {
-            overflow: visible;
+    def create(self, height=300, db_width=150, tbl_width=200, schema_width=700):
+        console = Output(layout={"height": "28px"})
+        schema = Output(
+            layout={
+                "height": _px(height),
+                "width": _px(schema_width),
+                "border": "1px solid grey",
+                "overflow": "scroll",
             }
-            </style>
-            """
-                )
+        )
+        data = Output()
+
+        tbl = TableList(self.spark, console, schema, data)
+        tbl.create(width=tbl_width, height=height)
+
+        db = DatabaseList(self.spark, console, tbl)
+        db.create(width=db_width, height=height)
+
+        display(
+            VBox(
+                [
+                    HBox(
+                        [
+                            VBox([db.refresh, db.list]),
+                            VBox([tbl.refresh, tbl.list]),
+                            VBox([console, schema]),
+                        ]
+                    ),
+                    data,
+                ]
             )
-
-    def __init__(self, spark=None):
-        """Singleton initializer
-        
-        Args:
-            spark (SparkSession, optional): Spark Session object. Defaults to None.
-        """
-        if not Databases.instance:
-            Databases.instance = Databases.__Databases(spark)
-
-    def __getattr__(self, name):
-        """Singleton getattr overload"""
-        return getattr(self.instance, name)
+        )
--- databrickslabs_jupyterlab/dbfs.py
+++ databrickslabs_jupyterlab/dbfs.py
@@ -1,188 +1,192 @@
 import os
 import re
 
+import pandas as pd
+
 from ipywidgets import Select, VBox, HBox, Button, Output
-from IPython.display import display
+from IPython.display import display, HTML, Markdown, Code, JSON, Image
 
 
 class Dbfs(object):
-    """Singleton for the DBFS object"""
+    """Database browser implementation
 
-    instance = None
+    Args:
+        dbutils (DBUtils): DBUtils object (for fs only)
+    """
 
-    class __Dbfs(object):
-        """Database browser implementation
-            
+    def __init__(self, dbutils):
+        self.dbutils = dbutils
+        self.running = False
+
+    def create(self, rows=30, path="/", height="400px"):
+        if self.running:
+            print("dbfs browser already running. Use close() first")
+            return
+        self.path = path
+        self.flist = Select(options=[], disabled=False, layout={"height": height})
+        self.flist.observe(self.on_click, names="value")
+
+        self.refresh = Button(icon="refresh", layout={"width": "40px"})
+        self.refresh.on_click(self.on_refresh)
+        self.path_view = Output()
+        self.preview = Output(
+            layout={
+                "width": "800px",
+                "height": height,
+                "overflow": "scroll",
+                "border": "1px solid gray",
+            }
+        )
+
+        self.up = Button(icon="arrow-up", layout={"width": "40px"})
+        self.up.on_click(self.on_up)
+
+        display(
+            VBox([HBox([self.refresh, self.up, self.path_view]), HBox([self.flist, self.preview]),])
+        )
+
+        self.update()
+        self.running = True
+
+    def convertBytes(self, fsize):
+        """Convert bytes to largest unit
+
         Args:
-            dbutils (DBUtils): DBUtils object (for fs only)
+            fsize (int): Size in bytes
+
+        Returns:
+            tuple: size of largest unit, largest unit
         """
+        size = fsize
+        unit = "B"
+        if size > 1024 * 1024 * 1024 * 10:
+            size = int(size / 1024.0 / 1024.0 / 1024.0)
+            unit = "GB"
+        elif size > 1024 * 1024 * 10:
+            size = int(size / 1024.0 / 1024.0)
+            unit = "MB"
+        elif size > 1024 * 10:
+            size = int(size / 1024.0)
+            unit = "KB"
+        return (size, unit)
 
-        def __init__(self, dbutils):
-            self.dbutils = dbutils
+    def update(self):
+        """Update the view when an element was selected"""
+        self.path_view.clear_output()
+        self.preview.clear_output()
+        with self.path_view:
+            print("updating ...")
+        try:
+            fobjs = self.dbutils.fs.ls(self.path)
+        except Exception as ex:
+            with self.path_view:
+                print("Error: Cannot access folder")
+            return False
 
-        def create(self, rows=30, path="/", height="400px"):
-            self.path = path
-            self.flist = Select(options=[], disabled=False, layout={"height": height})
-            self.flist.observe(self.on_click, names="value")
+        self.show_path(self.path)
 
-            self.refresh = Button(icon="refresh", layout={"width": "40px"})
-            self.refresh.on_click(self.on_refresh)
-            self.path_view = Output()
-            self.preview = Output(
-                layout={
-                    "width": "800px",
-                    "height": height,
-                    "overflow": "scroll",
-                    "border": "1px solid gray",
-                }
-            )
+        dirs = sorted([fobj.name for fobj in fobjs if fobj.isDir()], key=lambda x: x.lower())
+        files = sorted(
+            [
+                "%s (%d %s)" % ((fobj.name,) + self.convertBytes(fobj.size))
+                for fobj in fobjs
+                if not fobj.isDir()
+            ],
+            key=lambda x: x[0].lower(),
+        )
+        self.flist.options = [""] + dirs + files
+        return True
 
-            self.up = Button(icon="arrow-up", layout={"width": "40px"})
-            self.up.on_click(self.on_up)
+    def show_path(self, path):
+        """Show path in output widget
 
-            display(
-                VBox(
-                    [
-                        HBox([self.refresh, self.up, self.path_view]),
-                        HBox([self.flist, self.preview]),
-                    ]
-                )
-            )
+        Args:
+            path (str): Currently selected path
+        """
+        self.path_view.clear_output()
+        with self.path_view:
+            print("dbfs:" + re.sub(r"\s\(.*?\)$", "", path))
 
-            self.update()
+    def show_preview(self, path):
+        """Show preview of csv, md or txt in output widget
 
-        def convertBytes(self, fsize):
-            """Convert bytes to largest unit
-            
-            Args:
-                fsize (int): Size in bytes
-            
-            Returns:
-                tuple: size of largest unit, largest unit
-            """
-            size = fsize
-            unit = "B"
-            if size > 1024 * 1024 * 1024 * 10:
-                size = int(size / 1024.0 / 1024.0 / 1024.0)
-                unit = "GB"
-            elif size > 1024 * 1024 * 10:
-                size = int(size / 1024.0 / 1024.0)
-                unit = "MB"
-            elif size > 1024 * 10:
-                size = int(size / 1024.0)
-                unit = "KB"
-            return (size, unit)
-
-        def update(self):
-            """Update the view when an element was selected"""
-            self.path_view.clear_output()
+        Args:
+            path (str): Currently selected path
+        """
+        real_path = re.sub(r"\s\(.*?\)$", "", path)
+        parts = real_path.split(".")
+        if len(parts) > 0 and parts[-1].lower() in [
+            "md",
+            "html",
+            "csv",
+            "txt",
+            "sh",
+            "sql",
+            "py",
+            "scala",
+            "json",
+            "jpg",
+            "jpeg",
+            "png",
+            "gif",
+        ]:
+            ext = parts[-1].lower()
+            filename = "/dbfs" + real_path
+            # text = self.dbutils.fs.head(real_path)
             self.preview.clear_output()
-            with self.path_view:
-                print("updating ...")
-            try:
-                fobjs = self.dbutils.fs.ls(self.path)
-            except Exception as ex:
-                with self.path_view:
-                    print("Error: Cannot access folder")
-                return False
-
-            self.show_path(self.path)
-
-            dirs = sorted([fobj.name for fobj in fobjs if fobj.isDir()], key=lambda x: x.lower())
-            files = sorted(
-                [
-                    "%s (%d %s)" % ((fobj.name,) + self.convertBytes(fobj.size))
-                    for fobj in fobjs
-                    if not fobj.isDir()
-                ],
-                key=lambda x: x[0].lower(),
-            )
-            self.flist.options = [""] + dirs + files
-            return True
-
-        def show_path(self, path):
-            """Show path in output widget
-            
-            Args:
-                path (str): Currently selected path
-            """
-            self.path_view.clear_output()
-            with self.path_view:
-                print("dbfs:" + re.sub(r"\s\(.*?\)$", "", path))
-
-        def show_preview(self, path):
-            """Show preview of csv, md or txt in output widget
-            
-            Args:
-                path (str): Currently selected path
-            """
-            real_path = re.sub(r"\s\(.*?\)$", "", path)
-            parts = real_path.split(".")
-            if len(parts) > 0 and parts[-1].lower() in [
-                "md",
-                "csv",
-                "txt",
-                "sh",
-                "sql",
-                "py",
-                "scala",
-                "json",
-            ]:
-                text = self.dbutils.fs.head(real_path)
-                self.preview.clear_output()
-                with self.preview:
-                    print(text)
-
-        def on_refresh(self, b):
-            """Refresh handler
-            
-            Args:
-                b (ipywidgets.Button): clicked button
-            """
-            self.update()
-
-        def on_up(self, b):
-            """Up handler
-            
-            Args:
-                b (ipywidgets.Button): clicked button
-            """
-            new_path = os.path.dirname(self.path.rstrip("/"))
-            if new_path != self.path:
-                self.path = new_path
-            self.update()
-
-        def on_click(self, change):
-            """Click handler providing db and parent as context
-            
-            Args:
-                db (str): database name
-                parent (object): parent object
-            """
-            new_path = os.path.join(self.path, change["new"])
-            if change["old"] is not None:
-                if change["new"][-1] == "/":
-                    old_path = self.path
-                    self.path = new_path
-                    if not self.update():
-                        self.path = old_path
+            with self.preview:
+                if ext == "html":
+                    display(HTML(filename=filename))
+                elif ext in ["py", "sh", "sql", "scala"]:
+                    display(Code(filename=filename))
+                elif ext in ["jpg", "jpeg", "png", "gif"]:
+                    display(Image(filename=filename))
+                elif ext == "md":
+                    display(Markdown(filename=filename))
+                elif ext == "csv":
+                    df = pd.read_csv(filename)
+                    display(df)
                 else:
-                    self.show_path(new_path)
-                    self.show_preview(new_path)
+                    with open(filename, "r") as fd:
+                        print(fd.read())
 
-        def close(self):
-            """Close view"""
-            self.sc.close()
+    def on_refresh(self, b):
+        """Refresh handler
 
-    def __init__(self, dbutils=None):
-        """Singleton initializer
-        
         Args:
-            dbutils (DBUtils, optional): DBUtils object. Defaults to None.
+            b (ipywidgets.Button): clicked button
         """
-        if not Dbfs.instance:
-            Dbfs.instance = Dbfs.__Dbfs(dbutils)
+        self.update()
 
-    def __getattr__(self, name):
-        """Singleton getattr overload"""
-        return getattr(self.instance, name)
+    def on_up(self, b):
+        """Up handler
+
+        Args:
+            b (ipywidgets.Button): clicked button
+        """
+        new_path = os.path.dirname(self.path.rstrip("/"))
+        if new_path != self.path:
+            self.path = new_path
+        self.update()
+
+    def on_click(self, change):
+        """Click handler providing db and parent as context
+
+        Args:
+            db (str): database name
+            parent (object): parent object
+        """
+        new_path = os.path.join(self.path, change["new"])
+        if change["old"] is not None:
+            if len(change["new"]) > 0 and change["new"][-1] == "/":
+                old_path = self.path
+                self.path = new_path
+                if not self.update():
+                    self.path = old_path
+            else:
+                self.show_path(new_path)
+                self.show_preview(new_path)
+
+    def close(self):
+        """Close view"""
+        self.running = False
