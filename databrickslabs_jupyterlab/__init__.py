4e4f9b7adeebabfeaffc305e4fe319182630a2e8
Bernhard Walter <bernhard.walter@databricks.com>
2020-04-20 17:49:24 +0200

    fix that functions are always loaded

--- databrickslabs_jupyterlab/__init__.py
+++ databrickslabs_jupyterlab/__init__.py
@@ -1,39 +1,37 @@
 import os
+from notebook.utils import url_path_join
+
 from databrickslabs_jupyterlab._version import __version__, __version_info__
-
-if os.environ.get("DBJL_HOST") is None:
-
-    from databrickslabs_jupyterlab.status import KernelHandler, DbStartHandler, DbStatusHandler
-    from notebook.utils import url_path_join
-
-    def load_jupyter_server_extension(nbapp):
-        """
-        Called during notebook start
-        """
-        print("Loading server extension ...")
-        KernelHandler.nbapp = nbapp
-        base_url = nbapp.web_app.settings["base_url"]
-        status_route = url_path_join(base_url, "/databrickslabs-jupyterlab-status")
-        start_route = url_path_join(base_url, "/databrickslabs-jupyterlab-start")
-        nbapp.web_app.add_handlers(
-            ".*", [(status_route, DbStatusHandler), (start_route, DbStartHandler)]
-        )
+from databrickslabs_jupyterlab.status import KernelHandler, DbStartHandler, DbStatusHandler
 
 
-else:
+def load_jupyter_server_extension(nbapp):
+    """
+    Called during notebook start
+    """
+    print("Loading server extension ...")
+    KernelHandler.nbapp = nbapp
+    base_url = nbapp.web_app.settings["base_url"]
+    status_route = url_path_join(base_url, "/databrickslabs-jupyterlab-status")
+    start_route = url_path_join(base_url, "/databrickslabs-jupyterlab-start")
+    nbapp.web_app.add_handlers(
+        ".*", [(status_route, DbStatusHandler), (start_route, DbStartHandler)]
+    )
 
-    def is_remote():
-        """Check whether the current context is on the remote cluster
-        
-        Returns:
-            bool: True if remote else False
-        """
-        return os.environ.get("DBJL_HOST", None) is not None
 
-    def is_azure():
-        """Check whether the current context is on Azure Databricks
-        
-        Returns:
-            bool: True if Azure Databricks else False
-        """
-        return os.environ.get("DBJL_ORG", None) is not None
+def is_remote():
+    """Check whether the current context is on the remote cluster
+    
+    Returns:
+        bool: True if remote else False
+    """
+    return os.environ.get("DBJL_HOST", None) is not None
+
+
+def is_azure():
+    """Check whether the current context is on Azure Databricks
+    
+    Returns:
+        bool: True if Azure Databricks else False
+    """
+    return os.environ.get("DBJL_ORG", None) is not None
