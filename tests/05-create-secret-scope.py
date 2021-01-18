fa42affe75d2570d98ae1450b3db20824cd52d7c
Bernhard <b_walter@arcor.de>
2021-01-18 21:26:42 +0100

    add new mandatory parameters for create_scope

--- tests/05-create-secret-scope.py
+++ tests/05-create-secret-scope.py
@@ -15,5 +15,5 @@ except Exception as ex:  # pylint: disable=broad-except
     print(ex)
     sys.exit(1)
 
-client.create_scope("dbjl-pytest", None)
+client.create_scope("dbjl-pytest", None, None, None)
 client.put_secret("dbjl-pytest", "pytest-key", "databrickslabs-jupyterlab", None)
