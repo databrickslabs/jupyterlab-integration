ce557f2884be234d387ea87cce3cf669d63cea4b
Bernhard <b_walter@arcor.de>
2021-01-24 12:37:56 +0100

    started 2.1 version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if os.environ.get("DB_HOME") is None:
 
 setup(
     name="databrickslabs_jupyterlab",
-    version="2.0.0",
+    version="2.1.0-dev0",
     author="Bernhard Walter",
     author_email="bernhard.walter@databricks.com",
     url="https://github.com/databrickslabs/Jupyterlab-Integration",
